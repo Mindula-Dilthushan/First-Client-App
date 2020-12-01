9fe491bff83379ce692e87b865d8f7df05ad5839
Mindula-Dilthushan <66836116+Mindula-Dilthushan@users.noreply.github.com>
2020-12-01 09:42:26 +0530

    my first client app commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+            Socket socket = new Socket("127.0.0.0",5000);
+
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String message="" , reply="";
+
+            while (!message.equals("end")){
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                message=dataInputStream.readUTF();
+                System.out.println(message);
+            }
+
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
